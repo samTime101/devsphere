552deb65fa3f4a24ec320ea3f907a39cdb1440a1
samTime101 <srijanregmi44@gmail.com>
2025-08-25 17:35:54 +0545

    feat: working event display

--- index_app/views.py
+++ index_app/views.py
@@ -5,10 +5,17 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 from django.shortcuts import render
 from django.contrib import messages
+from sql_db.models import Event, EventImage
+
 
 class IndexView(LoginRequiredMixin, View):
     login_url = '/signin/'
 
     def get(self, request):
         messages.info(request, 'WELCOME TO HOME PAGE')
-        return render(request, 'index.html')
+        events = Event.objects.all().order_by('-date')[:3]
+        context = {
+            "events": events,
+
+        }
+        return render(request, 'index.html', context=context)
