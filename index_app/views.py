134d62f05a26700993f14e881b8080d50705c4cb
samTime101 <srijanregmi44@gmail.com>
2025-08-27 21:14:45 +0545

    feat: user info in index

--- index_app/views.py
+++ index_app/views.py
@@ -1,11 +1,14 @@
 # SAMIP REGMI
 # AUGUST 25
 
+# SAMIP REGMI
+# AUGUST 27
+# SENDING USER DETAILS 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 from django.shortcuts import render
 from django.contrib import messages
-from sql_db.models import Event
+from sql_db.models import Event , User
 
 
 class IndexView(LoginRequiredMixin, View):
@@ -14,8 +17,9 @@ class IndexView(LoginRequiredMixin, View):
     def get(self, request):
         messages.info(request, 'WELCOME TO HOME PAGE')
         events = Event.objects.all().order_by('-date')#[:3]
+        user = User.objects.get(username=request.user.username)
         return_context = {
             "events": events,
-
+            "user": user
         }
         return render(request, 'index.html', context=return_context)
